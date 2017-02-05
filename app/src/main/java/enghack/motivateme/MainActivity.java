543b5cf9c4d5d380540751c155786aadb419b425
Rowan Dempster <rowandempster@v1020-wn-8-164.campus-dynamic.uwaterloo.ca>
2017-02-04 21:33:17 -0500

    twitter

--- app/src/main/java/enghack/motivateme/MainActivity.java
+++ app/src/main/java/enghack/motivateme/MainActivity.java
@@ -2,6 +2,25 @@ package enghack.motivateme;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.util.List;
+
+import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.Scheduler;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Action;
+import io.reactivex.schedulers.Schedulers;
+import twitter4j.Paging;
+import twitter4j.Query;
+import twitter4j.QueryResult;
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.conf.ConfigurationBuilder;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +28,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ConfigurationBuilder cb = new ConfigurationBuilder();
+        cb.setDebugEnabled(true)
+                .setOAuthConsumerKey("vaZKWtXdTQCWhPFjiNAaJwuMz")
+                .setOAuthConsumerSecret("KQzAKgUC1Y31YRH2AvuHCSZPa83fq42tLgnn080UtMDo5KjMPp")
+                .setOAuthAccessToken("828006071377788929-YAmu75Lxyb8KzQ8MnNxiPQbJPhsrlgL")
+                .setOAuthAccessTokenSecret("X4Te52AsnPktMCABDsNyGgevHPxDDZKrylDWdC1YeE7FT");
+        TwitterFactory tf = new TwitterFactory(cb.build());
+        final Twitter twitter = tf.getInstance();
+        final Paging paging = new Paging(1, 1200);
+
+        Thread newThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    List<Status> statuses = twitter.getUserTimeline("Inspire_Us", paging);
+                    for(Status tweet : statuses){
+                        Log.d("asdf", "got tweet: " + tweet.getText());
+                    }
+                } catch (TwitterException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        newThread.start();
+
     }
 }
