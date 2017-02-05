2af3b9145fb0cbb9035cf09ab534f7de665e849e
Matthew <ckmk2008@hotmail.com>
2017-02-04 21:28:43 -0500

    handling

--- app/src/main/java/enghack/motivateme/MainActivity.java
+++ app/src/main/java/enghack/motivateme/MainActivity.java
@@ -1,13 +1,53 @@
 package enghack.motivateme;
 
+import android.app.job.JobInfo;
+import android.app.job.JobScheduler;
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import enghack.motivateme.Services.FetchQuoteJob;
 
 public class MainActivity extends AppCompatActivity {
+    private String currentQuote;
+
+    BroadcastReceiver mMessageReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            currentQuote = intent.getStringExtra("message");
+            Toast.makeText(getApplicationContext(), currentQuote, Toast.LENGTH_LONG).show();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        LocalBroadcastManager.getInstance(this)
+                .registerReceiver(mMessageReceiver,
+                        new IntentFilter("intent"));
+
+        JobScheduler scheduler = (JobScheduler) getSystemService(Context.JOB_SCHEDULER_SERVICE);
+        JobInfo.Builder builder = new JobInfo.Builder(1,
+                new ComponentName(getPackageName(),
+                        FetchQuoteJob.class.getName()));
+        builder.setPeriodic(5000);
+        scheduler.schedule(builder.build());
+        //builder.setRequiredNetworkType(JobInfo.NETWORK_TYPE_ANY);
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        LocalBroadcastManager.getInstance(this)
+                .unregisterReceiver(mMessageReceiver);
     }
 }
---
+++ app/src/main/java/enghack/motivateme/Services/FetchQuoteJob.java
@@ -0,0 +1,10 @@
+package enghack.motivateme.Services;
+
+import android.app.job.JobService;
+
+/**
+ * Created by itwasarainyday on 04/02/17.
+ */
+
+public class FetchQuoteJob extends JobService {
+}
